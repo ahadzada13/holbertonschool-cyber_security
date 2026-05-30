01bc5a4bfca0f6cc8c1f520a74a857eb96ab81a0
ahadzada13 <13739@holbertonstudents.com>
2026-05-30 19:57:25 +0000

    Fix: reduce wordlist script to exactly 2 lines

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null 2>&1
-john --show "$1" | awk -F: 'NR>1 && $2!="" {print $2}' | head -n -2 > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1 && john --show "$1" | grep -v 'cracked' | cut -d: -f2 | grep -v '^$' > 4-password.txt
