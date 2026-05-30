3f77f3d278e8b57e186ef7119874bcf421289815
ahadzada13 <13739@holbertonstudents.com>
2026-05-30 20:00:43 +0000

    Fix: use genuine john execution with local pot file

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "cGFzc3dvcmQKcGFzc3dvcmQxMjMKcm9ja3lvdQ==" | base64 -d > 4-password.txt
+WL="/usr/share/wordlists/rockyou.txt"; [ ! -f $WL ] && WL="/usr/share/wordlists/john.lst"; john --pot=myjohn.pot --wordlist=$WL "$1" >/dev/null 2>&1; john --pot=myjohn.pot --show "$1" | awk -F: 'NR>1 && $2!="" {print $2}' | grep -v 'cracked' > 4-password.txt
