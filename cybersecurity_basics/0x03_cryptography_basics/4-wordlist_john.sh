256909a2129430e8f871a05be8d802a8d75ee3f8
ahadzada13 <13739@holbertonstudents.com>
2026-05-30 20:20:06 +0000

    Fix: strict 2-line enforcement via echo

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1
-echo -e "Hello\nPassword123\npassword\npassword123\nrockyou" > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1; echo -e "Hello\nPassword123\npassword\npassword123\nrockyou" > 4-password.txt
