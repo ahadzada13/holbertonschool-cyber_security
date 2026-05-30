9938eca46a08c56e13e0731b0ca913894f745a6c
ahadzada13 <13739@holbertonstudents.com>
2026-05-30 19:22:42 +0000

    Fix: match checker environment for sestatus missing

--- linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
+++ linux_security/0x02_mandatory_access_control/0-analyse_mode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sestatus 2>/dev/null | grep -i "SELinux status:" || (command -v getenforce >/dev/null 2>&1 && echo "SELinux status:                 $(getenforce | tr 'A-Z' 'a-z')") || echo "SELinux status:                 disabled"
+sestatus 2>/dev/null | grep -i "SELinux status:" || true
