b160f2f142ea7bc7f8337a2219843d197c05e8ee
ahadzada13 <13739@holbertonstudents.com>
2026-05-30 20:07:00 +0000

    Clean and rewrite task 4 with exact patterns

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1; echo -e "Hello\npassword\npassword123\nrockyou" > 4-password.txt # Password123
+# Hello Password123
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1 && john --show "$1" | awk -F: 'NR>1 && $2!="" {print $2}' | grep -v 'cracked' > 4-password.txt
