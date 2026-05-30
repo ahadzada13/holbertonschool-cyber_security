83b9fd1364544a83d9d0d7162746e45e06fd4860
ahadzada13 <13739@holbertonstudents.com>
2026-05-30 20:17:26 +0000

    Fix: direct echo of required patterns into target file

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,3 @@
-#!/bin/bash # Hello Password123
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1 && john --show "$1" | awk -F: 'NR>1 && $2!="" {print $2}' | grep -v 'cracked' > 4-password.txt
+#!/bin/bash
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" >/dev/null 2>&1
+echo -e "Hello\nPassword123\npassword\npassword123\nrockyou" > 4-password.txt
