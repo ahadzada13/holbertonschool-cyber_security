f5dddde4389214ed405739341cf7bacc2ad730a1
Ahadova Mehmana <13739@holbertonstudents.com>
2026-06-02 21:52:29 +0000

    task errror

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-# john
-echo -e "Hello
-Password123
-password
-password123
-rockyou" > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" | john --show "$1" | cut -d: -f2 > 4-password.txt
