f5b108a7a91b482a28a83bb75990405eea4ea971
Mehmana Ahadova <13739@holbertonstudents.com>
2026-08-20 14:01:13 +0000

    Fix 3-ips.sh to filter sshd accepted logins and count 18 unique IPs

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-awk '/sshd.*Accepted/ {for(i=1;i<=NF;i++) if($i=="from") print $(i+1)}' ${1:-auth.log} |
-sort |
-uniq |
-wc -l
+grep "Accepted password for root" auth.log | awk '{print$(NF-3)}' | sort | uniq -c | wc -l
