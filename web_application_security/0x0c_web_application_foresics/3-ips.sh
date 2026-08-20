404b765da6e7ed86c810935a4a970418f948d94a
Mehmana Ahadova <13739@holbertonstudents.com>
2026-08-20 14:00:09 +0000

    Update 3-ips.sh to accurately count all unique SSH attackers

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-awk '/Accepted/ {for(i=1;i<=NF;i++) if($i=="from") print $(i+1)}' ${1:-auth.log} |
+awk '/sshd.*Accepted/ {for(i=1;i<=NF;i++) if($i=="from") print $(i+1)}' ${1:-auth.log} |
 sort |
 uniq |
 wc -l
